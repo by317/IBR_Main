7338ba293b6fb804020f4e4917f69dca0f0a9787
Ben York <york.ben@gmail.com>
2011-12-07 15:24:50 -0500

    Deleted Debug Folder

--- Debug/source/RingBuff.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-source/RingBuff.obj: ../source/RingBuff.c
-source/RingBuff.obj: C:/Users/benyork/git/IBR_Main/include/RingBuff.h
-
-../source/RingBuff.c: 
-C:/Users/benyork/git/IBR_Main/include/RingBuff.h: 
